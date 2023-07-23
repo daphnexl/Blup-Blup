883af67266bb5a76d9af6c71f372286636800283
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2023-07-23 20:31:36 +0300

    skor ve sütunlar

--- Assets/scripts/kuş.cs
+++ Assets/scripts/kuş.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class kuş : MonoBehaviour
 {
     public float zıplama;
 
     Rigidbody2D rb;
     GameScript gameScript;
+    public TMP_Text skor_text;
+    public float skor;
 
     void Start() {
         gameScript = GetComponent<GameScript>();
+        skor = 0;
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     
@@ -19,11 +24,20 @@ public class kuş : MonoBehaviour
         if(Input.GetMouseButtonDown(0)) {
             rb.velocity = Vector2.up * zıplama;
         }
+        skor_text.text = skor.ToString();
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Zemin") || other.gameObject.CompareTag("Boru")) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Zemin") || other.gameObject.CompareTag("Boru"))
+        {
             gameScript.GameOver();
         }
+        else if (other.gameObject.CompareTag("Scorer"))
+        {
+            skor++;
+        }
     }
+        
+
 }
